affa268dfbb90af3739d7e12d144d03d78989c55
Danilo de Morais <danilo.m.gustavo@gmail.com>
2025-01-25 11:38:11 -0300

    Atualizacao no tamanho do arquivo para importar

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ const PDFDocument = require('pdfkit');
 // CONFIGURAÇÃO DO EXPRESS
 // --------------------------------------------------------------------------------
 const app = express();
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 app.use(cors({ origin: '*' }));
 
 // --------------------------------------------------------------------------------
